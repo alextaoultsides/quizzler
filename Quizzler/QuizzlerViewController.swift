8ebb3030203ec1dd1febecae098926c00e2f9729
alextaoultsides <alextaoultsides@gmail.com>
2018-09-05 11:30:03 -0400

    Feat: Answer all questions and restart quiz

--- Quizzler/QuizzlerViewController.swift
+++ Quizzler/QuizzlerViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class QuizzlerViewController: UIViewController {
     
     //Place your instance variables here
-    let questionsList = QuestionBank().list
+    var questionsList = QuestionBank().list
+    var pickedAnswer: Bool = false
+    var correctAnswers: Int = 0
     var randNum: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -21,22 +23,33 @@ class QuizzlerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scoreLabel.text = "0"
+        progressLabel.text = "0"
         updateUI()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else {
+            pickedAnswer = false
+        }
         checkAnswer()
         nextQuestion()
     }
     
     func randomQuestion(){
-        randNum = Int.random(in: 0...questionsList.count-1)
+        if questionsList.count > 1 {
+            randNum = Int.random(in: 0 ... questionsList.count-1)
+        } else {
+            randNum = 0
+        }
     }
     
     
-    
     func updateUI() {
+        
         randomQuestion()
         let question = questionsList[randNum]
         questionLabel.text = question.questionText
@@ -45,17 +58,35 @@ class QuizzlerViewController: UIViewController {
     
 
     func nextQuestion() {
-        updateUI()
+        if questionsList.count == 1 {
+            let alert = UIAlertController(title: "End of Quiz", message: "Would you like to play again?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                self.startOver()
+            }))
+            self.present(alert, animated: true, completion: nil)
+            
+        } else {
+            questionsList.remove(at: randNum)
+            updateUI()
+        }
     }
     
     
     func checkAnswer() {
+        if pickedAnswer == questionsList[randNum].answer {
+            correctAnswers += 1
+            scoreLabel.text = "\(correctAnswers)"
+        }
         
     }
     
     
     func startOver() {
-       
+        progressLabel.text = "0"
+        scoreLabel.text = "0"
+        correctAnswers = 0
+        questionsList = QuestionBank().list
+        updateUI()
     }
     
 
